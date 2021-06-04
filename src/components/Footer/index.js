3b4c48a07bbcd8804ba0e288039f53839a76b7ed
Nate J <jamis.walsh@gmail.com>
2021-06-04 11:50:39 -0700

    small changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,13 +5,13 @@ function Footer() {
     <footer className="footer container mt-5">
         <i className="nes-ash align-bottom animate"></i>
           <div className="nes-balloon from-left">
-              <a href="https://github.com/NathanJamis">
+              <a href="https://github.com/NathanJamis" target="_blank">
                 <span className="nes-icon github align-bottom"></span>
               </a>
-              <a href="mailto:jamis.walsh@gmail.com">
+              <a href="mailto:jamis.walsh@gmail.com" target="_blank">
                 <span className="nes-icon gmail align-bottom"></span>
               </a>
-              <a href="https://www.linkedin.com/in/nathanjamis/">
+              <a href="https://www.linkedin.com/in/nathanjamis/" target="_blank">
                 <span className="nes-icon linkedin"></span>
               </a>
           </div>
