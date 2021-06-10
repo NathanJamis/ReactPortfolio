6bb794dad30bc5ed890b59e505fc5d7a31aa15b2
Nate J <jamis.walsh@gmail.com>
2021-06-10 13:00:48 -0700

    small text changes

--- src/components/AboutCard/index.js
+++ src/components/AboutCard/index.js
@@ -8,11 +8,10 @@ function AboutCard() {
         <p className="title nes-container">About</p>
           <div className="card-body">
           <img className="img-responsive float-left mb-3" width="250px" height="260px" src={headshot}></img>
-
-
-          <p className="mt-3">Hello there! Your friendly neighborhood web-dev here!</p>
-          <p>I am a creator. I love to create, make, and build. My wide artistic taste allows me to appreciate all eras, styles, and forms of art. On the other side of my brain, my need to analyze and seek logical solutions to problems. I've learned how to combine them and find my path.</p>
-          <p>For so long, I have wanted to learn to code, and now that dream is a reality. I am currently learning web development through the University of Washington, and I am ready to start building the next great web app!</p>
+          <p className="mt-3">Hello there!</p>
+          <p>Your friendly neighborhood web-dev here.</p>
+          <p>I am a creator. I love to create, make, and build. My wide artistic taste allows me to appreciate all eras, styles, and forms of art. On the other side of my brain, I have a strong desire to analyze and seek logical solutions to problems. I've learned how to combine them and find my path.</p>
+          <p>For so long, I have wanted to learn to code, and now that dream is a reality. I recently received my certification in Full-Stack Web Development through the University of Washington, and I am ready to start building the next great web app!</p>
           </div>
       </div>
     </div>
